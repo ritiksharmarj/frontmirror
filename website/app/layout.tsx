41a33a15c09a12738c50f058da8a89e2cfb96659
ritiksharmarj <theonlyritik@gmail.com>
2023-10-04 15:35:15 +0530

    frontmirror website

---
+++ website/app/layout.tsx
@@ -0,0 +1,22 @@
+import './globals.css';
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export const metadata: Metadata = {
+  title: 'Front Mirror - One click camera check',
+  description: 'A one-click camera check, right from the extension bar.',
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang='en'>
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
---
+++ website/app/page.tsx
@@ -0,0 +1,3 @@
+export default function Home() {
+  return <div>Front Mirror</div>;
+}
