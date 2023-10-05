3cb18cb6925e2bd5645714b02a2c9cdb5f905c87
ritiksharmarj <theonlyritik@gmail.com>
2023-10-06 00:40:14 +0530

    metadata changes

--- website/app/layout.tsx
+++ website/app/layout.tsx
@@ -8,6 +8,44 @@ const inter = Inter({ subsets: ['latin'] });
 export const metadata: Metadata = {
   title: 'Front Mirror - One click camera check',
   description: 'A one-click camera check, right from the chrome toolbar.',
+  openGraph: {
+    title: 'Front Mirror - One click camera check',
+    description: 'A one-click camera check, right from the chrome toolbar.',
+    url: 'https://frontmirror.ritiksharma.me/',
+    siteName: 'Front Mirror - One click camera check',
+    images: [
+      {
+        url: 'https://frontmirror.ritiksharma.me/og.png',
+        width: 1600,
+        height: 840,
+      },
+    ],
+    locale: 'en_US',
+    type: 'website',
+  },
+  icons: {
+    shortcut: '/favicons/favicon.ico',
+    icon: [
+      {
+        url: '/favicons/favicon-16x16.png',
+        sizes: '16x16',
+        type: 'image/png',
+      },
+      {
+        url: '/favicons/favicon-32x32.png',
+        sizes: '32x32',
+        type: 'image/png',
+      },
+    ],
+    apple: [
+      {
+        url: '/favicons/apple-touch-icon.png',
+        sizes: '180x180',
+        type: 'image/png',
+      },
+    ],
+  },
+  themeColor: '#1c1c1c',
 };
 
 export default function RootLayout({
@@ -17,38 +55,6 @@ export default function RootLayout({
 }) {
   return (
     <html lang='en'>
-      <head>
-        <link
-          rel='shortcut icon'
-          href='/favicons/favicon.ico'
-          type='image/x-icon'
-        />
-        <link
-          rel='apple-touch-icon'
-          sizes='180x180'
-          href='/favicons/apple-touch-icon.png'
-        />
-        <link
-          rel='icon'
-          type='image/png'
-          sizes='32x32'
-          href='/favicons/favicon-32x32.png'
-        />
-        <link
-          rel='icon'
-          type='image/png'
-          sizes='16x16'
-          href='/favicons/favicon-16x16.png'
-        />
-        <link rel='manifest' href='/favicons/site.webmanifest' />
-        <link
-          rel='mask-icon'
-          href='/favicons/safari-pinned-tab.svg'
-          color='#5bbad5'
-        />
-        <meta name='msapplication-TileColor' content='#da532c' />
-        <meta name='theme-color' content='#1c1c1c' />
-      </head>
       <body className={inter.className}>
         {children}
         <Analytics />
