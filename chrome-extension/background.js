d81cb0ecb3a901de3f1e68ae844c9e06b2821396
ritiksharmarj <theonlyritik@gmail.com>
2023-10-06 01:22:11 +0530

    uninstall action in background

--- chrome-extension/background.js
+++ chrome-extension/background.js
@@ -1,8 +1,20 @@
+// When click on extension icon
 chrome.action.onClicked.addListener((tab) => {
   // Open the app by sending a message to the content script
   chrome.tabs.sendMessage(tab.id, { openApp: true });
 });
 
+// Use keyboard shortcut to open extension
 chrome.commands.onCommand.addListener((command, tab) => {
   chrome.tabs.sendMessage(tab.id, { openApp: true });
 });
+
+// Open URL in new tab when extension is installed
+chrome.runtime.onInstalled.addListener((details) => {
+  if (details.reason === 'install') {
+    chrome.tabs.create({ url: 'https://frontmirror.ritiksharma.me/' });
+  }
+});
+
+// Open survey URL in new tab when extension is uninstalled
+chrome.runtime.setUninstallURL('https://ritiksharma.me/feedback');
