a69684d8c217d8b507e55f4254164bb870de2a8b
ritiksharmarj <theonlyritik@gmail.com>
2025-02-15 01:29:15 +0530

    Initialize project structure with TypeScript, Webpack, and essential files

---
+++ webpack.config.ts
@@ -0,0 +1,86 @@
+import path from 'path';
+import webpack from 'webpack';
+import CopyWebpackPlugin from 'copy-webpack-plugin';
+
+const NODE_ENV = (process.env.NODE_ENV ||
+  'development') as webpack.Configuration['mode'];
+
+const isDevelopment = NODE_ENV === 'development';
+
+const { dir, ...options } = isDevelopment
+  ? { dir: 'dev', devtool: 'cheap-module-source-map' }
+  : { dir: 'prod' };
+
+const fileExtensions = [
+  'eot',
+  'gif',
+  'jpeg',
+  'jpg',
+  'otf',
+  'png',
+  'svg',
+  'ttf',
+  'woff',
+  'woff2',
+];
+
+const config: webpack.Configuration = {
+  mode: NODE_ENV,
+  entry: {
+    background: './src/background/index.ts',
+    content: './src/content/index.ts',
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, `dist/${dir}`),
+    clean: true,
+    publicPath: '',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: new RegExp(`\\.(${fileExtensions.join('|')})$`),
+        type: 'asset/resource',
+        generator: {
+          filename: 'assets/[name][ext]',
+        },
+      },
+    ],
+  },
+  plugins: [
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: 'manifest.json',
+          to: '.',
+          transform: function (content) {
+            // generates the manifest file using the package.json informations
+            return Buffer.from(
+              JSON.stringify({
+                description: process.env.npm_package_description,
+                version: process.env.npm_package_version,
+                ...JSON.parse(content.toString()),
+              })
+            );
+          },
+        },
+        { from: 'src/assets', to: 'assets' },
+      ],
+    }),
+  ],
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js', '.jsx'],
+  },
+  ...options,
+};
+
+export default config;
